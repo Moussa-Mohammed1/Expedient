8ee7f3512962b9d348468845db78875c3f2f861f
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-15 18:42:26 +0000

    feat(migrations): add post_images table with foreign key to posts

--- backend/database/migrations/2026_03_15_184057_create_post_images_table.php
+++ backend/database/migrations/2026_03_15_184057_create_post_images_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('post_images', function (Blueprint $table) {
             $table->id();
             $table->string('content');
-            $table->softDeletes();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('community_id')->constrained('communities');
+            $table->foreignId('post_id')->constrained('posts');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('post_images');
     }
 };
