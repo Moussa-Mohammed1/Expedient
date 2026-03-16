07ee6550ff45daa9335fbb019e672a19c55853a5
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-16 00:07:26 +0000

    feat(migrations): create salles and galleries tables with foreign key relationships

---
+++ backend/database/migrations/2026_03_15_235020_create_salles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('tagline')->nullable();
+            $table->string('sessionType')->nullable();
+            $table->string('description')->nullable();
+            $table->integer('existenceYears')->nullable();
+            $table->string('city');
+            $table->foreignId('coach_id')->constrained('users');
+            $table->foreignId('sport_id')->constrained('sports');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salles');
+    }
+};
