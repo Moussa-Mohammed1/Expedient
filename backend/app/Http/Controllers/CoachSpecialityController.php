b0745ee5a2e724a4f26bb424f686f6a2216f2e85
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-16 01:17:37 +0000

    feat(controllers): add CoachSpeciality and Speciality controllers with basic CRUD methods feat(models): create CoachSpeciality and Speciality models feat(migrations): create specialities and coach_specialities tables with foreign key relationships

---
+++ backend/app/Http/Controllers/CoachSpecialityController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CoachSpeciality;
+use Illuminate\Http\Request;
+
+class CoachSpecialityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CoachSpeciality $coachSpeciality)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CoachSpeciality $coachSpeciality)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CoachSpeciality $coachSpeciality)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CoachSpeciality $coachSpeciality)
+    {
+        //
+    }
+}
