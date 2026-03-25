b1ae2c254f4363f7f87d2ca73f327db09058233d
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-25 14:40:21 +0100

    feat(model): enhance Post model with relationships and mass assignable attributes

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -2,9 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'content',
+        'user_id',
+        'community_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function community(): BelongsTo
+    {
+        return $this->belongsTo(Community::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(PostImages::class, 'post_id');
+    }
+
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
 }
