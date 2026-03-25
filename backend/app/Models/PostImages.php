c91ecedd0d14e9a106bd8cc58bb3df055009baf6
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-25 15:00:50 +0100

    feat(model): create PostImages model with relationships and mass assignable attributes

--- backend/app/Models/PostImages.php
+++ backend/app/Models/PostImages.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PostImages extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'post_id',
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
