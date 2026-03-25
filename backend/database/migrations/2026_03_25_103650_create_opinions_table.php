4a17a520f97a43473b549a218adc40380084e4f8
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-25 11:45:47 +0100

    feat(migration): create opinions table for user ratings and feedback

---
+++ backend/database/migrations/2026_03_25_103650_create_opinions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('opinions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('coach_id')->constrained('coaches');
+            $table->decimal('rate')->default(0);
+            $table->string('content')->nullable();
+            $table->boolean('isAllowed')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('opinions');
+    }
+};
