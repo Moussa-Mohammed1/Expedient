5e68b359a1877cb60a24e3dab6c6663b7cfc5baa
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-17 09:21:54 +0100

    feat(migrations): create favoris table and rename it to favorites for consistency

--- backend/database/migrations/2026_04_16_150511_create_favoris_table.php
+++ backend/database/migrations/2026_04_16_150511_create_favoris_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('favorite', function (Blueprint $table) {
+        Schema::create('favoris', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('salle_id')->constrained('salles');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('favorite');
+        Schema::dropIfExists('favoris');
     }
 };
