103a13cf20a4bf1e0678a092d551ef80b6272239
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-19 17:31:02 +0100

    feat(app-logo): wrap logo text in a link to home route

--- backend/resources/views/components/app-logo.blade.php
+++ backend/resources/views/components/app-logo.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <h1 class="my-4 text-center text-2xl font-bold text-white">Expedient.</h1>
+    <h1 class="my-4 text-center text-2xl font-bold text-white"><a href="{{ route('home') }}">Expedient.</a></h1>
 </div>
