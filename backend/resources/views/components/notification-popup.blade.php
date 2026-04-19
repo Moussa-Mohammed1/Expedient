466babeefb9b139caae4abbcea44345f7705d64c
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-19 17:31:10 +0100

    feat(notification-popup): enhance notification styles and animations for success and error messages

--- backend/resources/views/components/notification-popup.blade.php
+++ backend/resources/views/components/notification-popup.blade.php
@@ -1,19 +1,52 @@
 <div id="popup">
-    @if(session('success'))
-        <div class="fixed top-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    @if(session('error'))
-        <div class="fixed top-5 right-5 bg-yellow-500 text-black px-4 py-2 rounded shadow">
-            {{ session('error') }}
-        </div>
-    @endif
-
     @if(session('message'))
-        <div class="fixed top-5 right-5 bg-yellow-500 text-black px-4 py-2 rounded shadow">
+        <div class="fixed top-5 right-5 bg-yellow-500 text-black text-sm sm:text-base px-4 py-2 rounded shadow">
             {{ session('message') }}
         </div>
     @endif
 </div>
+@if (session('success'))
+    <div id="success-notification" class="fixed top-6 z-50 pointer-events-none left-1/2 text-sm sm:text-lg -translate-x-1/2
+                        bg-yellow-500 backdrop-blur-sm text-black font-semibold px-7 py-3 rounded-full shadow-lg
+                         border border-white/10
+                        opacity-0 -translate-y-5
+                        transition-all duration-700 ease-in-out truncate">
+
+        {{ session('success') }}
+    </div>
+@endif
+
+@if (session('error'))
+    <div id="error-notification" class="fixed top-6 z-50 pointer-events-none left-1/2 text-sm sm:text-lg -translate-x-1/2
+                        bg-red-900/80 text-red-100 px-7 py-3 rounded-full shadow-lg
+                        backdrop-blur-md border border-red-300/30
+                        opacity-0 -translate-y-5
+                        transition-all duration-700 ease-in-out">
+
+        {{ session('error') }}
+    </div>
+@endif
+
+
+<script>
+    const animateNotification = (elementId) => {
+        const notification = document.getElementById(elementId);
+
+        if (!notification) {
+            return;
+        }
+
+        requestAnimationFrame(() => {
+            notification.classList.remove('opacity-0', '-translate-y-5', 'pointer-events-none');
+            notification.classList.add('opacity-100', 'translate-y-0', 'pointer-events-auto');
+        });
+
+        setTimeout(() => {
+            notification.classList.remove('opacity-100', 'translate-y-0', 'pointer-events-auto');
+            notification.classList.add('opacity-0', '-translate-y-5', 'pointer-events-none');
+        }, 3000);
+    };
+
+    animateNotification('success-notification');
+    animateNotification('error-notification');
+</script>
