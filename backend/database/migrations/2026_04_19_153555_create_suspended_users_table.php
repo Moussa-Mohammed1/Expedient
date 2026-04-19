7b66e6b5976994d4ae8c5a4a9555fb51f4407f83
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-19 17:27:22 +0100

    feat(migration): create suspended_users table for managing user suspensions

---
+++ backend/database/migrations/2026_04_19_153555_create_suspended_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suspended_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('suspended_by')->constrained('users');
+            $table->string('reason');
+            $table->enum('status', ['active', 'expired']);
+            $table->dateTime('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suspended_users');
+    }
+};
