f50581d99f3915a42beda4116e10ed87cb6a2dff
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-19 17:35:26 +0100

    feat(routes): add routes for favorites, community membership, posts, comments, reports, and coach specialities

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Coach\CoachController;
 use App\Http\Controllers\Coach\CoachVerificationController;
@@ -7,13 +9,20 @@ use App\Http\Controllers\Coach\SalleController as CoachSalleController;
 use App\Http\Controllers\ExploreController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Coach\SpecialityController as CoachSpecialityController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Community\CommunityController;
+use App\Http\Controllers\Community\CommentController;
+use App\Http\Controllers\Community\LikeController as CommunityLikeController;
+use App\Http\Controllers\Community\MembershipController;
+use App\Http\Controllers\Community\PostController as CommunityPostController;
 use App\Http\Controllers\User\RoleController;
 use App\Http\Controllers\Sport\SportController;
 use App\Http\Controllers\Salle\SalleController;
+use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\OpinionController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\Coach\OpinionController as CoachOpinionController;
 
 use App\Models\Trainee;
@@ -37,12 +46,33 @@ Route::middleware('auth')->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
     Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
     Route::post('/coach-verifications', [CoachVerificationController::class, 'store'])->name('coach-verifications.store');
+    Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
+    Route::post('/favorites/{salle}', [FavoriteController::class, 'store'])->name('favorites.store');
+    Route::delete('/favorites/{salle}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
     Route::resource('communities', CommunityController::class);
+    Route::post('/communities/{community}/join', [MembershipController::class, 'store'])->name('communities.join');
+    Route::delete('/communities/{community}/leave', [MembershipController::class, 'destroy'])->name('communities.leave');
+    Route::get('/posts/create', [CommunityPostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [CommunityPostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}', [CommunityPostController::class, 'show'])->name('posts.show');
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::put('/posts/{post}/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/posts/{post}/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    Route::post('/posts/{post}/like', [CommunityLikeController::class, 'store'])->name('posts.like');
     Route::resource('roles', RoleController::class);
     Route::resource('sports', SportController::class);
     Route::resource('salles', SalleController::class);
     Route::resource('coaches', CoachController::class);
     Route::post('/coaches/{coach}/opinions', [OpinionController::class, 'store'])->name('opinions.store');
+
+    //reports
+
+    Route::post('/reports', [ReportController::class, 'store'])->name('reports.store');
+    Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+    Route::patch('/reports/{report}/cancel', [ReportController::class, 'cancel'])->name('reports.cancel');
+    
+    //User with opinions
+    
     Route::get('/opinions/{opinion}', [OpinionController::class, 'show'])->name('opinions.show');
     Route::put('/opinions/{opinion}', [OpinionController::class, 'update'])->name('opinions.update');
     Route::delete('/opinions/{opinion}', [OpinionController::class, 'destroy'])->name('opinions.destroy');
@@ -50,12 +80,28 @@ Route::middleware('auth')->group(function () {
 
 
     Route::middleware('coach')->group(function () {
+
+        //salles 
+
         Route::get('/coach/salles', [CoachSalleController::class, 'index'])->name('coach.salles');
         Route::get('/coach/salles/create', [CoachSalleController::class, 'create'])->name('coach.salles.create');
         Route::get('/coach/salles/{salle}/edit', [CoachSalleController::class, 'edit'])->name('coach.salles.edit');
         Route::put('/coach/salles/{salle}', [CoachSalleController::class, 'update'])->name('coach.salles.update');
         Route::delete('/coach/salles/{salle}', [CoachSalleController::class, 'destroy'])->name('coach.salles.destroy');
         Route::post('/coach/salles', [CoachSalleController::class, 'store'])->name('coach.salles.store');
-        Route::get('/coach/opinions' , [CoachOpinionController::class, 'index'])->name('coach.opinions');
+        
+        //Coach with opinions
+
+        Route::get('/coach/opinions', [CoachOpinionController::class, 'index'])->name('coach.opinions');
+        
+        //Specialities
+
+        Route::get('/specialities', [CoachSpecialityController::class, 'index'])->name('coach.specialities.index');
+        Route::post('/specialities', [CoachSpecialityController::class, 'store'])->name('coach.specialities.store');
+        Route::put('/specialities/{speciality}', [CoachSpecialityController::class, 'update'])->name('coach.specialities.update');
+        Route::delete('/specialities/{speciality}', [CoachSpecialityController::class, 'destroy'])->name('coach.specialities.destroy');
     });
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/users', [UserController::class, 'index']);
 });
