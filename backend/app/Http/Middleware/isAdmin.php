fa6656bab49b2670f8d548c7cb6c0398ece369f4
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-21 23:33:47 +0100

    fix(middleware): update isAdmin middleware to use role_id for authorization check

--- backend/app/Http/Middleware/isAdmin.php
+++ backend/app/Http/Middleware/isAdmin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +16,7 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user()->role !== 'admin') {
+        if (auth()->user()->role_id !== Role::where('title', 'admin')->value('id')) {
             return redirect('/home');
         }
         return $next($request);
