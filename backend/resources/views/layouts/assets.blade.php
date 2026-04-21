4e2258c1d7a284778e030accd8823b221e4b2642
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-21 10:34:49 +0100

    fix(assets): add favicon link to layout

--- backend/resources/views/layouts/assets.blade.php
+++ backend/resources/views/layouts/assets.blade.php
@@ -1,3 +1,4 @@
 @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 @endif
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
